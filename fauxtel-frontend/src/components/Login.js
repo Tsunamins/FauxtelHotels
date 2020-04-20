db76867ffa15709fa396232b71bc80d513c2cfbf
Tsunamins <reillyamr@gmail.com>
2020-04-20 11:37:54 -0400

    import & incorp basic use of action for loginForm

--- fauxtel-frontend/src/components/Login.js
+++ fauxtel-frontend/src/components/Login.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import {connect} from 'react-redux'
 //import of action for login form
+import updateLoginForm from '../actions/loginForm.js'
 
-const Login = (props) => {
+const Login = (props, {updateLoginForm}) => {
     return (
         <form>
-            <input type="text" name="username" value={props.username} placeholder="username"></input>
-            <input type="password" name="password" value={props.password} placeholder="password"></input>
+            <input value={props.username} onClick={updateLoginForm} type="text" name="username"  placeholder="username"></input>
+            <input value={props.password} onClick={updateLoginForm} type="password" name="password"  placeholder="password"></input>
             <input type="submit" value="Log In"></input>
 
         </form>
     )
 }
+
+
 
