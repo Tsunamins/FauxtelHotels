e802d5452a19a1b921c69ca3c95a64cac03260ed
Tsunamins <reillyamr@gmail.com>
2020-04-20 11:42:43 -0400

    create reducer for loginForm and add to store

--- fauxtel-frontend/src/store.js
+++ fauxtel-frontend/src/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, compose, combineReducers} from 'redux'
 import thunk from 'redux-thunk';
 import usersReducer from './reducers/usersReducer.js'
+import loginForm from './reducers/loginForm.js'
 
 const reducer = combineReducers({
-    users: usersReducer
+    users: usersReducer, 
+    loginForm
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
