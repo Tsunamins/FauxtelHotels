8db81f9860647c173a3026a7c026eda950f50538
Tsunamins <reillyamr@gmail.com>
2020-04-22 17:39:11 -0400

    clean up store w relavent reducers

--- fauxtel-frontend/src/store.js
+++ fauxtel-frontend/src/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware, compose, combineReducers} from 'redux'
 import thunk from 'redux-thunk';
 import usersReducer from './reducers/usersReducer.js'
-import loginForm from './reducers/loginForm.js'
+
 import currentUser from './reducers/currentUser.js'
 
 const reducer = combineReducers({
     users: usersReducer, 
-    loginForm,
+   
     currentUser
 })
 
