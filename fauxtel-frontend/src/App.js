448506dfef84f3df340b015ccb73ee6e9dc910dd
Tsunamins <reillyamr@gmail.com>
2020-04-22 17:38:46 -0400

    add logout button with conditions of login

--- fauxtel-frontend/src/App.js
+++ fauxtel-frontend/src/App.js
@@ -3,39 +3,58 @@ import logo from './fauxtellogo.svg';
 import './App.css';
 import { connect } from 'react-redux'
 import Login from './components/Login.js'
-import { getCurrentUser } from './actions/currentUser.js'
+import Logout from "./components/Logout.js"
+import { getCurrentUser,  } from './actions/currentUser.js'
 
 //function App() {
 class App extends React.Component {
 
-  // componentDidMount(){
-  //   getCurrentUser()
+  componentDidMount =() => {
+    this.props.getCurrentUser()
+   
 
-  // }
+  }
 
   render(){
-
+    const { loggedIn} = this.props
+    const currentUser = this.state
+    
+  
+    
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Login />
-    </div>
+    
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Edit <code>src/App.js</code> and save to reload.</p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+        </header>
+
+        
+        <Login />
+        
+        
+        <h2>{ loggedIn ? <><p>Logged in as {currentUser}</p><Logout/></> : "Not logged in"}</h2>
+      
+      </div>
+
   );
 
   }
 }
 
-export default App;
+// const mapDispatchToProps = dispatch => ({
+//   getCurrentUser: () => dispatch(getCurrentUser())
+// })
+
+const mapStateToProps = state => {
+  console.log(state.currentUser)
+  return ({
+    
+    loggedIn: !!state.currentUser
+   
+  })
+}
+
+export default connect(mapStateToProps, {getCurrentUser})(App);
