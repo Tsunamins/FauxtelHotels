cf5740804d77e30d692f7166dde9fb5ad3a354d0
Tsunamins <reillyamr@gmail.com>
2020-04-22 17:41:34 -0400

    changed naming

--- fauxtel-frontend/src/reducers/currentUser.js
+++ fauxtel-frontend/src/reducers/currentUser.js
@@ -1,10 +1,12 @@
+
 export default (state = null, action) => {
     switch (action.type) {
-      case "SET_CURRENT_USER":
-       
-        return action.user
-      case "CLEAR_CURRENT_USER":
+      case "LOGIN_USER":
+        return action.user      
+     
+      case "LOGOUT_USER":
         return null
+
       default:
           
         return state
