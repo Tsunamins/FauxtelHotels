87b395db23b9acd9d1b329536a0553e619abf8f5
Tsunamins <reillyamr@gmail.com>
2020-04-23 10:53:08 -0400

    add authorized status to users_controller

--- fauxtel-backend/app/controllers/api/v1/users_controller.rb
+++ fauxtel-backend/app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  skip_before_action :authorized, only: [:create]
 
   # GET /users
   def index
@@ -22,9 +22,10 @@ class Api::V1::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      token = encode_token({id: @user.id})
+      render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { message: 'Invalid username or password' }, status: :unauthorized
     end
   end
 
