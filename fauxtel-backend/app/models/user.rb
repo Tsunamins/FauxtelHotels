40c4dc4da6409aee9f6780809aacb4738d895ad2
Tsunamins <reillyamr@gmail.com>
2020-04-23 10:53:19 -0400

    add validations to user model

--- fauxtel-backend/app/models/user.rb
+++ fauxtel-backend/app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :reservations
     has_many :rooms, through: :reservations
+
+    validates :email, :first_name, :last_name, presence: true
+    validates :email, uniqueness :ture
 end
