ed2faf834597f58602b9463c7fc532fe07a75bbb
Tsunamins <reillyamr@gmail.com>
2020-04-23 13:15:17 -0400

    correct spelling, remove authorize for now

--- fauxtel-backend/app/controllers/application_controller.rb
+++ fauxtel-backend/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  before_action :authorized
+  #before_action :authorized
     # def user_object(user)
     #     UserSerializer.new(user)
     # end
--- fauxtel-backend/app/models/user.rb
+++ fauxtel-backend/app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+    
     has_many :reservations
     has_many :rooms, through: :reservations
 
     validates :email, :first_name, :last_name, presence: true
-    validates :email, uniqueness :ture
+    validates :email, uniqueness: true
 end
