595da73b02a636c37269c76c35758df9aa25dd51
Tsunamins <reillyamr@gmail.com>
2020-04-26 15:59:31 -0400

    console.log(store), fix naming based on this

--- fauxtel-frontend/src/components/Rooms.js
+++ fauxtel-frontend/src/components/Rooms.js
@@ -1,18 +1,47 @@
 import React from 'react'
 import {Route, Link} from 'react-router-dom' //enables link tags
+import { connect } from 'react-redux'
+
+import { getRooms } from '../actions/getRooms.js'
+
+class Rooms extends React.Component {
+
+    // state = {}
+    
+     
+    
+    // vote = (id) => {
+    //   this.state[id] ? this.setState({[id]: this.state[id] += 1}) : this.setState({[id]: 1})
+    // }
+    
+    render() {
+      return (
+          <div>
+            {this.props.rooms.rooms.map(room =>
+              <li key={room.id}>{room.room_number}</li>
+            )}
+          </div>
+        )
+      }
+    }
+    
+    export default connect(null, {getRooms})(Rooms)
 
 
-const Rooms = (props) => {
 
-  return (
-    props
-    // before this looked as above, a basic passing in of account componnent was used:
-    //<div>
-    //  {props.accounts.map(account => 
-    //    <li key={account.id}>Account account={account/></li>})}
-    //</div>
 
-  )
-}
+// const Rooms = (props) => {
 
-export default Accounts
+//     return (
+//       <div>
+//         {/* {props.rooms.map(room =>
+//           <li key={room.id}>
+//             {room.number}
+//           </li> )} */}
+//       </div>
+     
+  
+//     )
+//   }
+  
+//   export default Rooms
---
+++ fauxtel-frontend/src/containers/BookNav.js
@@ -0,0 +1,53 @@
+// going to display a link to Book Now
+// or going to change the route of the page to booking interface
+//going to have available - data regarding login? maybe not sure
+//links are going to be specific to reservation#create in backend
+import React from 'react';
+import { connect } from 'react-redux'
+import { Link, NavLink, Route, Switch } from 'react-router-dom'
+import BookNavHome from '../components/BookNavHome.js'
+import BookNow from '../components/BookNow.js'
+import Rooms from '../components/Rooms.js'
+import { getRooms } from '../actions/getRooms.js'
+
+class BookNav extends React.Component {
+
+    componentDidMount() {
+        this.props.getRooms()
+    }       
+
+    render(){
+
+    return (
+        <div className="BookNav">
+           {/* will need some kind of logic to hide this once using it */}
+           {/* some of this is temporary for viewing functions of these things */}
+           <Switch>
+                <Route path="/booknow">
+                    <BookNow />
+                </Route>
+                <Route path="/rooms" render={(routerProps) => <Rooms {...routerProps} rooms={this.props.rooms}/>} >
+                   
+                </Route>
+            </Switch>
+      
+           
+            <BookNavHome />
+              
+        </div>
+    )
+}
+}
+
+
+const mapStateToProps = state => {
+    console.log(state.rooms)
+    return ({
+        rooms: state.rooms
+      
+     
+    })
+  }
+  
+  export default connect(mapStateToProps, {getRooms})(BookNav);
+
