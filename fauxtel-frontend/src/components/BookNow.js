a6fe21e7b80b4408c075a9a4f6e39a4a863bdf41
Tsunamins <reillyamr@gmail.com>
2020-04-26 19:29:47 -0400

    narrow down some rooms to work with

--- fauxtel-frontend/src/components/BookNow.js
+++ fauxtel-frontend/src/components/BookNow.js
@@ -3,13 +3,29 @@ import { connect } from 'react-redux'
 import {Route, Link} from 'react-router-dom' //enables link tags
 import { getRooms } from '../actions/getRooms.js'
 
-const BookNow = (props) => {
-    return(
-        <div>Some booking interface to display here</div>
-    )
- 
-}
 
+    class BookNow extends React.Component {
+       
+       
+        render() {
+            const allRooms = this.props.rooms.rooms
 
+            const locationOne = []
+            for (let i = 0; i < allRooms.length; i++){
+                if(allRooms[i].location_id === 1)
+                locationOne.push(allRooms[i])
+            }
+            let locationTotal = locationOne.length
+            console.log(locationTotal)
+          return (
+              <div>
+                {this.props.rooms.rooms.map(room =>
+                  <li key={room.id}>{room.room_number}</li>
+                )}
+              </div>
+            )
+          }
+        }
+        
+        export default connect(null, {getRooms})(BookNow)
 
-export default BookNow
