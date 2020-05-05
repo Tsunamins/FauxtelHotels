01624c1380af0961662bf5bf2f6a9b875b2a2581
Tsunamins <reillyamr@gmail.com>
2020-05-05 17:47:07 -0400

    just meant to keep as example

--- fauxtel-frontend/src/components/BookRoomsData.js
+++ fauxtel-frontend/src/components/BookRoomsData.js
@@ -1,31 +1,60 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import {Route, Link} from 'react-router-dom' //enables link tags
+import { connect } from 'react-redux'
 import { getRooms } from '../actions/getRooms.js'
 
 
-    class BookRoomsData extends React.Component {
-       
-       
-        render() {
-            const allRooms = this.props.rooms.rooms
 
-            const locationOne = []
-            for (let i = 0; i < allRooms.length; i++){
-                if(allRooms[i].location_id === 1)
-                locationOne.push(allRooms[i])
-            }
-            let locationTotal = locationOne.length
-            console.log(locationTotal)
-          return (
-              <div>
-                {this.props.rooms.rooms.map(room =>
-                  <li key={room.id}>{room.room_number}</li>
-                )}
-              </div>
-            )
-          }
+
+// const Rooms = (props) => {
+//   console.log(props)
+
+//   return (
+//     <div>
+//       {props.rooms.rooms.map(room =>
+//         <li key={room.id}>
+//           {room.room_number}
+//         </li> )}
+//     </div>
+
+//   )
+// }
+
+// // const mapStateToProps = state => {
+// //   return {
+// //     rooms: state.rooms
+// //   }
+// // }
+
+// export default connect(null, {getRooms})(Rooms)
+
+
+//previous
+class Rooms extends React.Component {
+
+  render() {
+    // new
+      const allRooms = this.props.rooms.rooms
+      const locationOne = []
+      for (let i=0; i < allRooms.length; i++){
+        if(allRooms[i].attributes.location_id === 1){
+          locationOne.push(allRooms[i])
         }
-        
-        export default connect(null, {getRooms})(BookRoomsData)
+      }
+      let locationTotal = locationOne.length
+      console.log(locationOne)
 
+
+    console.log(this.props.rooms.rooms[0])
+    return (
+      // previous
+        <div>
+          {this.props.rooms.rooms.map(room =>
+              <li key={room.attributes.id}>{room.attributes.room_number}</li>
+          )}
+        </div>
+      )
+    }
+  }
+  
+  export default connect(null, {getRooms})(Rooms)
