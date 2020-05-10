467b6a3aa1b5a9fc8e5f888e32d0c8eb3b091b40
Tsunamins <reillyamr@gmail.com>
2020-05-10 13:13:01 -0400

    match avail rooms w room id's & rooms w no reservs

--- fauxtel-frontend/src/components/BookNow.js
+++ fauxtel-frontend/src/components/BookNow.js
@@ -64,12 +64,15 @@ class BookNow extends React.Component {
     }
   }
 
-  handleShowRooms(from, to, reservations){
+  handleShowRooms(from, to, reservations, rooms){
 
     let reservation_build = toDateRange(from, to)
-    console.log(reservation_build)
     sessionStorage.setItem("reservation_build", reservation_build)
-    let found = findAvailability(reservation_build, reservations)
+    let no_conflict_room_ids = determineAvailability(reservation_build, reservations)
+    let match1 = matchAvailableRooms(no_conflict_room_ids, rooms)
+    let match2 = roomsNoReservations(rooms)
+
+
 
 
 
@@ -83,9 +86,8 @@ class BookNow extends React.Component {
   }
 
   render() {
-    console.log(this.props)
+    const rooms = this.props.rooms.rooms
     const reservations = this.props.reservations.reservations
-    //const reservations = this.props.reservation_build.reservations
     const today = new Date();
     const { from, to, enteredTo } = this.state;
     const modifiers = { start: from, end: enteredTo };
@@ -112,7 +114,7 @@ class BookNow extends React.Component {
                 ${to.toLocaleDateString()}`}{' '}
           {from && to && (
         
-        <button className="link" onClick={this.handleShowRooms(from, to, reservations)}>
+        <button className="link" onClick={this.handleShowRooms(from, to, reservations, rooms)}>
           Show Rooms
         </button>
       )}
@@ -155,36 +157,73 @@ const toDateRange = function(start, end){
   return formatArray
 }
 
-function findAvailability(reservation_build, reservations){
+function determineAvailability(reservation_build, reservations){
   let no_conflict = []
-
-  
+  let isolate_room_id = []
   for(let i = 0; i < reservations.length; i++){
       //so finds when there is date overlap - returns true - so "true" means room will not be available
       //returns false when there is no date overlap and the rooms would be available
       //also remember at this time, only location 1 has reservations, so if, later, incorporating room avail, will have to re-filter locations where appl
       let date_check = findDateOverlap(reservation_build, reservations[i].date_range)
-      console.log(date_check)
       if (date_check === false){
         no_conflict.push(reservations[i])
+        isolate_room_id.push(reservations[i].room_id.toString())
       }
-      
-
-
-  
-     
-    
   }
-  console.log(no_conflict)
+  return isolate_room_id
 }
 
 function findDateOverlap(reservation_build, just_res_array) { 
   return reservation_build.some(item => just_res_array.includes(item)) 
 } 
 
+function matchAvailableRooms(no_conflict_room_ids, rooms){
+  //match reservation room_id's with room.id of all rooms
+  let room_available = []
+  for(let i = 0; i < no_conflict_room_ids.length; i++){
+    
+    for(let j = 0; j < no_conflict_room_ids.length; j++){
+      console.log(typeof no_conflict_room_ids[i])
+      console.log(typeof rooms[j].id)
+      if(no_conflict_room_ids[i] === rooms[j].id){
+        room_available.push(rooms[i])
+      }
+    }
+  }
+  console.log(room_available)
+  return room_available
+}
+
+function roomsNoReservations(rooms){
+
+  //using extra loop to filter out location 1 for now, would modify if adding more locations to expand app later, or even add additional param to specify location
+  let loc1 = []
+  for(let i = 0; i < rooms.length; i++){
+    if(rooms[i].attributes.location_id === 1){
+      loc1.push(rooms[i])
+    }
+
+    console.log(rooms[i].attributes.location_id)
+    console.log(rooms[i].attributes.reservations) //prob looking to filter where array.length is < 1
+  }
+
+  let room_available = []
+  for(let j = 0; j < loc1.length; j++){
+    if(loc1[j].attributes.reservations.length < 1){
+      room_available.push(loc1[j])
+    }
+  }
+  console.log(room_available)
+  return (room_available)
+}
+
+
+
+
+
+
 
 const mapStateToProps = state => {
-  console.log(state)
   return {
     //reservations: state.reservations,
     rooms: state.rooms,
