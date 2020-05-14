cacbb064ad39de3f891317cc46b1c5bde5ae2561
Tsunamins <reillyamr@gmail.com>
2020-05-14 13:48:18 -0400

    build a reserve component

---
+++ fauxtel-frontend/src/components/Reserve.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const Reserve = (props) => {
+  console.log(props)
+  return (
+   
+      <div>
+        Form to reserve
+        <form>
+
+        </form>
+    
+      </div>
+      
+  )
+}
+
+export default Reserve
+  
