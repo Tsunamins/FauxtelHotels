89a223258e48e316032e2c815ea04a2c523c116e
Tsunamins <reillyamr@gmail.com>
2020-05-14 20:21:16 -0400

    add add reservation to reducer type

--- fauxtel-frontend/src/reducers/reservations.js
+++ fauxtel-frontend/src/reducers/reservations.js
@@ -1,7 +1,9 @@
 export default function reservations(state = {reservations: []}, action) {
     switch (action.type) {
       case 'GET_RESERVATIONS':  
-        return {...state, reservations:  action.reservations}     
+        return {...state, reservations:  action.reservations}
+      case 'ADD_RES':
+        return state.concat(action.res)     
         
         default:
             return state
