83d76f8365425c5eecbc51b350c9217b435b1e17
Tsunamins <reillyamr@gmail.com>
2020-05-17 16:21:19 -0400

    add userReserv to store ability but prob delete

--- fauxtel-frontend/src/reducers/buildReservation.js
+++ fauxtel-frontend/src/reducers/buildReservation.js
@@ -1,11 +1,12 @@
-export default function buildReservation(state = {room: [], dates: []}, action) {
+export default function buildReservation(state = {room: [], dates: [], allResv: []}, action) {
  
   switch (action.type) {
     case "SET_ROOM_SELECTION":  
       return {...state, room:  [action.room]}
   // case "SET_DATES":
   //     return {...state, dates: action.dates}
-      
+    case "SET_USER_RESERVATIONS":
+      return {...state, allResv: [action.resv]}
       default:
           return state
       }
