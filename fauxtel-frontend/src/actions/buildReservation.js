174094a0af72af30478435e27f37fe3658e2cd26
Tsunamins <reillyamr@gmail.com>
2020-05-17 16:19:49 -0400

    add getUserReserv, not needed,, prob delete later

--- fauxtel-frontend/src/actions/buildReservation.js
+++ fauxtel-frontend/src/actions/buildReservation.js
@@ -6,13 +6,20 @@ export const buildReservation = room => {
     }
   }
 
+export const setUserReservations = resv => {
+    return {
+      type: "SET_USER_RESERVATIONS",
+      resv
+    }
+}
+
 // export const startOver = data => {
 //   return {
 //     type: "START_OVER"
 //   }
 // }
 
-  export const getRoom = (room_id) => {
+export const getRoom = (room_id) => {
       
     //return dispatch (built in) as an argument, a thunk function 
     return dispatch => {
@@ -31,10 +38,24 @@ export const buildReservation = room => {
         }
 }
 
-// export const setDates = dates => {
-//     console.log(dates)
-//     return {
-//       type: "SET_DATES",
-//       dates
-//     }
-//   }
+export const getUserReservations = (user_id) => {
+  return dispatch => {
+  const token = localStorage.getItem("token")
+    fetch(`http://localhost:3000/api/v1/users/${user_id}`, {
+      headers: {
+        "Authorization": token
+      }
+    })
+      .then(resp => resp.json())
+ 
+      .then(response => {
+        if (response.error) {
+          alert("Not authorized - user reservations")
+        } else {
+          console.log(response.data.attributes.reservations)
+            dispatch(setUserReservations(response.data.attributes.reservations))
+        }
+          })      
+      .catch(console.log)
+}
+}
