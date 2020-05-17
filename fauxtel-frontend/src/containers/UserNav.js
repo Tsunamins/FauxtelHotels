1633ade0f262c494ca75940b874ea8d99f153169
Tsunamins <reillyamr@gmail.com>
2020-05-17 16:20:57 -0400

    add userReserv route

--- fauxtel-frontend/src/containers/UserNav.js
+++ fauxtel-frontend/src/containers/UserNav.js
@@ -5,6 +5,8 @@ import UserNavLinks from '../components/UserNavLinks.js'
 import Logout from '../components/Logout.js'
 import SignUp from '../components/SignUp.js'
 import Login from '../components/Login.js'
+import UserResv from '../components/UserReservations.js'
+import { getUserReservations } from '../actions/buildReservation.js'
 
 
 
@@ -23,7 +25,14 @@ const UserNav = ({currentUser, loggedIn}) => {
             {/* this UserNavHome sets up the links for these routes to refer to */}
             {/* other wise the links in this component need to be listed out further here */}
             <UserNavLinks />
-            <h2>{ loggedIn ? <><p>Logged in as {currentUser.attributes.first_name}</p><Logout/></> : "Not logged in"}</h2>    
+            <h2>{ loggedIn ? 
+                    <>
+                        <p>Logged in as {currentUser.attributes.first_name}</p>
+                        <Logout />
+                        <Route path="/view-reservations"> <UserResv /></Route>
+                    </> 
+                : 
+                    "Not logged in"}</h2>    
         </div>
     )
 }
@@ -38,6 +47,6 @@ const mapStateToProps = ({currentUser}) => {
      
     })
   }
-  
+  //{getUserReservations}
   export default connect(mapStateToProps)(UserNav);
 
