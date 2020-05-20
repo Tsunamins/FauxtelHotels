cbd936fb909e9900ef3868650019057b5ccdadf8
Tsunamins <reillyamr@gmail.com>
2020-05-20 16:31:10 -0400

    set up token to auth delete

--- fauxtel-frontend/src/actions/reservations.js
+++ fauxtel-frontend/src/actions/reservations.js
@@ -11,6 +11,14 @@ export const addReservation = res => {
     res
   }
 }
+
+export const deleteReservation = res => {
+  console.log(res)
+  return {
+    type: "DELETE_RES",
+    res
+  }
+}
   
 export const getReservations = () => {  
     return dispatch => {
@@ -28,7 +36,7 @@ export const getReservations = () => {
   }
 
 export const createReservation = (res_info) => {
-  console.log(res_info)
+
     return dispatch => {
       return fetch('http://localhost:3000/api/v1/reservations', {
         method: "POST",
@@ -49,3 +57,85 @@ export const createReservation = (res_info) => {
     }
 }
 
+export const cancelReservation = (res_id) => {
+  console.log(res_id)
+  const token = localStorage.getItem("token")
+  if(token){
+     return dispatch => {
+    
+      return fetch(`http://localhost:3000/api/v1/reservations/${res_id}`, {
+    
+      method: "DELETE",
+      headers: {
+        "Authorization": token
+      }
+    })
+      .then(resp => {
+        if (resp.error){
+          alert(resp.error)
+        }else {
+     
+           dispatch(deleteReservation(res_id))
+           alert("Reservation Cancelled")
+      
+   
+      
+    }
+  })
+      .catch(error => console.log(error))
+
+  }
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const cancelReservation = (resID) => {
+//   console.log(typeof resID)
+//   return dispatch => {
+//   // const token = localStorage.getItem("token")
+//   // console.log(token)
+//   console.log(resID)
+
+//     return fetch(`http://localhost:3000/api/v1/reservations/${resID}`, {
+//       method: "DELETE",
+//       headers: {
+
+//         // "Authorization": token,
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//       }
+//     })
+//       .then(resp => resp.json())
+ 
+//       .then(response => {
+      
+//         if (response.error) {
+//           alert(response.error)
+//         } else {
+//           console.log(response)
+//             dispatch(deleteReservation(response))
+//         }
+//           })      
+//       .catch(console.log)
+// }
+// }
