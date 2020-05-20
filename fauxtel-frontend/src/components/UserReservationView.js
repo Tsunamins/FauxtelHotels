daabb8a30d4557f872312d36caf328a2362a95d8
Tsunamins <reillyamr@gmail.com>
2020-05-20 19:16:56 -0400

    add state and BookNow comp to Reserv View

--- fauxtel-frontend/src/components/UserReservationView.js
+++ fauxtel-frontend/src/components/UserReservationView.js
@@ -3,6 +3,8 @@ import {connect, useDispatch} from 'react-redux'
 import {Link, Redirect} from 'react-router-dom'
 import ModifyReservation from './ModifyReservation.js'
 import {cancelReservation} from '../actions/reservations.js'
+import {modifyReservation} from '../actions/reservations.js'
+import BookNow from './BookNow.js'
 
 import { getRoom } from '../actions/buildReservation.js'
 
@@ -11,40 +13,63 @@ import { getRoom } from '../actions/buildReservation.js'
 //const UserResvView = ({ res }) => {
   class UserResvView extends React.Component {  
   //const dispatch = useDispatch();
-  componentDidMount(){
-   
-    this.props.getRoom()
-  }
+    state = {
+      renderModify: false
+    }
 
   handleCancel = () => {
     
     this.props.cancelReservation(this.props.res.id)
     this.props.history.push("/")
   }
+
+  handleModify = () => {
+    console.log(this.state)
+    console.log(this.props)
+    this.setState({
+      renderModify: true
+
+
+
+    })
+   
+  }
     
   render(){
     console.log(this.state)
     console.log(this.props)
-    
+    const modify = this.state.renderModify
     const res = this.props.res
     console.log(res)
     // const room = this.props.getRoom(res.room_id)
     // console.log(room)
   return (
 
-    
-     res ?
+   
+      res ?
+
       <div>
-        <h3>Location: {res.attributes.location.name}</h3>
-        <p>Room Type: {res.attributes.room.room_type}</p>
-        <p>From: {res.attributes.start_date}</p>
-        <p>To: {res.attributes.end_date}</p>
-        <Link to={`/modify-reservation/${res.id}/edit`}>Modify Reservation</Link>
-        <br></br>
-        <br></br>
-       <button onClick={this.handleCancel}>Cancel Reservation</button>
-      </div> :
-      <p>!Res</p>
+         { modify ? <><BookNow/></> : null}
+       
+        <div>
+         
+          
+          <h3>Location: {res.attributes.location.name}</h3>
+          <p>Room Type: {res.attributes.room.room_type}</p>
+          <p>From: {res.attributes.start_date}</p>
+          <p>To: {res.attributes.end_date}</p>
+          <Link to={`/modify-reservation/${res.id}/edit`}>Modify Reservation</Link>
+          <button onClick={this.handleModify}>Modify Reservation</button>
+          <br></br>
+          <br></br>
+          <button onClick={this.handleCancel}>Cancel Reservation</button>
+        </div>
+
+      </div> 
+       :
+       <p>!Res</p>
+      
+
   )
 }
 }
@@ -60,7 +85,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-      getRoom: () => { dispatch(getRoom()) },
+      modifyReservation: () => { dispatch(modifyReservation()) },
       cancelReservation: () => { dispatch(cancelReservation()) },
       
   }
