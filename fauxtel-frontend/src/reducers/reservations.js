2349af8c7a2295edb9e2de81edf23df87f8cad9d
Tsunamins <reillyamr@gmail.com>
2020-05-20 16:33:05 -0400

    add delete reservation to reducer

--- fauxtel-frontend/src/reducers/reservations.js
+++ fauxtel-frontend/src/reducers/reservations.js
@@ -1,10 +1,13 @@
 export default function reservations(state = {reservations: []}, action) {
+  console.log(action)
     switch (action.type) {
       case 'GET_RESERVATIONS':  
         return {...state, reservations:  action.reservations}
       case 'ADD_RES':
          
-        return {...state, reservations: [...state.reservations, action.res]}   
+        return {...state, reservations: [...state.reservations, action.res]}
+      case 'DELETE_RES':
+        return  state.reservations.filter(res => res.id === action.res ? false : true)
         
         default:
             return state
