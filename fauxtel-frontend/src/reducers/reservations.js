2a28c8b80ee86c7db5adccf833604fe68bf09e3b
Tsunamins <reillyamr@gmail.com>
2020-05-21 16:05:29 -0400

    add update reservation to store

--- fauxtel-frontend/src/reducers/reservations.js
+++ fauxtel-frontend/src/reducers/reservations.js
@@ -1,15 +1,15 @@
 export default function reservations(state = {reservations: []}, action) {
- 
+   
     switch (action.type) {
       case 'GET_RESERVATIONS':  
-        return {...state, reservations:  action.reservations}
+        return action.reservations
       case 'ADD_RES':
          
-        return {...state, reservations: [...state.reservations, action.res]}
+        return state.concat(action.res)
       case 'DELETE_RES':
-        return  state.reservations.filter(res => res.id === action.res ? false : true)
+        return  state.filter(res => res.id === action.res ? false : true)
       case 'UPDATE_RES':
-        return state.reservations.map(res => res.id === action.res.id ? action.res : res)
+        return state.map(res => res.id === action.res.id ? action.res : res)
         
         default:
             return state
