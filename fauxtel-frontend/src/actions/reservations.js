f193fbf70162f763f6f2215a1046610f7845b028
Tsunamins <reillyamr@gmail.com>
2020-05-21 16:03:24 -0400

    fix errors in updating a reservation

--- fauxtel-frontend/src/actions/reservations.js
+++ fauxtel-frontend/src/actions/reservations.js
@@ -13,7 +13,7 @@ export const addReservation = res => {
 }
 
 export const deleteReservation = res => {
-  console.log(res)
+
   return {
     type: "DELETE_RES",
     res
@@ -21,6 +21,8 @@ export const deleteReservation = res => {
 }
 
 export const updateReservation = res => {
+
+  console.log(res)
   return {
     type: "UPDATE_RES",
     res
@@ -66,7 +68,7 @@ export const createReservation = (res_info) => {
 }
 
 export const cancelReservation = (res_id) => {
-  console.log(res_id)
+
   const token = localStorage.getItem("token")
   if(token){
      return dispatch => {
@@ -97,25 +99,29 @@ export const cancelReservation = (res_id) => {
 }
 
 
-export const modifyReservation = (res_id) => {
-  console.log(res_id)
+export const modifyReservation = (resv_id, resv_data) => {
+  console.log(resv_id)
+  console.log(resv_data)
   const token = localStorage.getItem("token")
   if(token){
      return dispatch => {
     
-      return fetch(`http://localhost:3000/api/v1/reservations/${res_id}`, {
+      return fetch(`http://localhost:3000/api/v1/reservations/${resv_id}`, {
     
       method: "PATCH",
       headers: {
-        "Authorization": token
-      }
+        "Authorization": token,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(resv_data)
     })
-      .then(resp => {
-        if (resp.error){
-          alert(resp.error)
+      .then(resp => resp.json())
+      .then(response => {
+        if (response.error){
+          alert(response.error)
         }else {
-     
-           dispatch(updateReservation(res_id))
+          console.log(response.data)
+           dispatch(updateReservation(response.data))
            alert("Reservation Updated")
       
    
